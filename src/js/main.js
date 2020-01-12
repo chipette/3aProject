d01b7ed7966c077883f73436bf2f5723c1e3f13e
Charline <charline@iMac-de-moi.local>
2020-01-12 22:31:54 +0100

    finition css section services, ajout js pour smooth scroll et back to top

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,37 @@ import 'bootstrap'; // j'importe bootstrap.js depuis node_modules
 
 $(document).ready(function () {
 
-  
+  //Smooth scroll
+  function scrollNav() {
+    $('.navbar a').click(function(){  
+      //Toggle Class
+      $(".active").removeClass("active");      
+      $(this).closest('li').addClass("active");
+      var theClass = $(this).attr("class");
+      $('.'+theClass).parent('li').addClass('active');
+      //Animate
+      $('html, body').stop().animate({
+          scrollTop: $( $(this).attr('href') ).offset().top - 60
+      }, 400);
+      return false;
+    });
+    $('.scrollTop a').scrollTop();
+  }
+  scrollNav();
+
+
+  //Back to top
+  $(window).scroll(function(){ 
+    if ($(this).scrollTop() > 3600) { 
+        $('#scroll').fadeIn(); 
+    } else { 
+        $('#scroll').fadeOut(); 
+    } 
+  }); 
+  $('#scroll').click(function(){ 
+    $("html, body").animate({ scrollTop: 0 }, 600); 
+    return false; 
+  }); 
+
 
 });
